bdc24359df3e35cb7ad29b88fc61cc86afb86bae
Eduardo Poleo <eduardopoleo@gmail.com>
2022-07-06 19:23:42 -0400

    Reading from firebase

--- src/components/lists.js
+++ src/components/lists.js
@@ -1,19 +1,39 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore"; 
 
-let id = 2
+const firebaseConfig = {
+  apiKey: "AIzaSyDiFemQgvz_sDf3iziKaaqY0wAbE9ecbco",
+  authDomain: "todosreact-a18e8.firebaseapp.com",
+  projectId: "todosreact-a18e8",
+  storageBucket: "todosreact-a18e8.appspot.com",
+  messagingSenderId: "206861045069",
+  appId: "1:206861045069:web:fd67de15b3180e2c38fb04",
+  measurementId: "G-313XXJYC4L"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 export default function Lists() {
   const [lists, setLists] = useState([])
   const [newList, setNewList] = useState('')
-  const fetchedlists = [
-    { id: 1, name: 'Groceries' },
-    { id: 2, name: 'Clothing' } 
-  ]
 
   useEffect(() => {
-    setLists(fetchedlists)    
+    const fetchLists = async () => {
+      let dbLists = await getDocs(collection(db, "lists"));
+      let myLists = [];
+      dbLists.forEach((list) => myLists.push({ id: list.id, ...list.data() }))
+      console.log("myLists", myLists)
+      setLists(myLists)
+    }
+
+    fetchLists()
+    console.log("lists", lists)
   }, [])
 
+  let id;
   const onAddNewList = () => {
     id++
     const newListEntry = { name: newList, id: id }
@@ -31,7 +51,6 @@ export default function Lists() {
     if (event.key === 'Enter')
       onAddNewList()
   }
-  
   return(
     <>
       <input 
