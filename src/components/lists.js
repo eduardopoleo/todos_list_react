e097c0ccaca139d80bfbcb31f3214c63aaa7225b
Eduardo Poleo <eduardopoleo@gmail.com>
2022-07-07 08:19:53 -0400

    wip

--- src/components/lists.js
+++ src/components/lists.js
@@ -1,20 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { collection, getDocs } from "firebase/firestore"; 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDiFemQgvz_sDf3iziKaaqY0wAbE9ecbco",
-  authDomain: "todosreact-a18e8.firebaseapp.com",
-  projectId: "todosreact-a18e8",
-  storageBucket: "todosreact-a18e8.appspot.com",
-  messagingSenderId: "206861045069",
-  appId: "1:206861045069:web:fd67de15b3180e2c38fb04",
-  measurementId: "G-313XXJYC4L"
-};
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import { db } from '../firebase'
 
 export default function Lists() {
   const [lists, setLists] = useState([])
@@ -25,19 +12,20 @@ export default function Lists() {
       let dbLists = await getDocs(collection(db, "lists"));
       let myLists = [];
       dbLists.forEach((list) => myLists.push({ id: list.id, ...list.data() }))
-      console.log("myLists", myLists)
       setLists(myLists)
     }
-
     fetchLists()
-    console.log("lists", lists)
   }, [])
 
-  let id;
   const onAddNewList = () => {
-    id++
-    const newListEntry = { name: newList, id: id }
-
+    const newListEntry = { name: newList }
+    console.log('weird', newListEntry)
+    const saveList = async (newListEntry) => {
+      console.log('weird2', newListEntry)
+      return await addDoc(collection(db, "lists"), { ...newListEntry } );
+    }
+    const result = saveList()
+    console.log('final result ', result)
     setLists([newListEntry, ...lists])
 
     setNewList('')
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDiFemQgvz_sDf3iziKaaqY0wAbE9ecbco",
+  authDomain: "todosreact-a18e8.firebaseapp.com",
+  projectId: "todosreact-a18e8",
+  storageBucket: "todosreact-a18e8.appspot.com",
+  messagingSenderId: "206861045069",
+  appId: "1:206861045069:web:fd67de15b3180e2c38fb04",
+  measurementId: "G-313XXJYC4L"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
