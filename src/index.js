a7503f8bb37c8a834e3fadb5bb3815a7b437d489
Eduardo Poleo <eduardopoleo@gmail.com>
2022-07-24 13:08:34 -0400

    Private Routes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import SignUp from './components/signUp'
 import Login from './components/login'
 import { AuthProvider } from "./contexts/AuthContext";
 import Layout from "./components/layout";
+import PrivateRoute from "./components/PrivateRoute";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,11 +16,11 @@ root.render(
       <AuthProvider>
         <Layout>
           <Routes>
-            <Route path="/" element={<Lists />} />
-            <Route path="sign_up" element={<SignUp/>}/>
-            <Route path="login" element={<Login/>}/>
-            <Route path="lists/:listId" element={<List />} />
-            <Route path="lists" element={<Lists />}/>
+            <Route path="/" element={<PrivateRoute><Lists/></PrivateRoute>} />
+            <Route path="/sign_up" element={<SignUp/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/lists/:listId" element={<PrivateRoute><List/></PrivateRoute>} />
+            <Route path="/lists" element={<PrivateRoute><Lists/></PrivateRoute>}/>
           </Routes>
         </Layout>
       </AuthProvider>
